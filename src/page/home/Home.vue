5ede34f1039856be18ade4c90f32cca02b107ca2
winner <1750110604@qq.com>
2018-06-28 17:04:16 +0800

    编辑城市列表页

---
+++ src/page/home/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <home-header
+      :LoginIcon="true"
+      :HomeLogo="true"
+    >
+    </home-header>
+    <home-location></home-location>
+  </div>
+</template>
+<script>
+import HomeHeader from '@/common/Header'
+import HomeLocation from './components/HomeLocation'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeLocation
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/page/home/components/HomeLocation.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="item top"></div>
+    <div class="item bottom"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeLocation',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~style/variable.scss';
+  .container{
+    background: white;
+    @include wh(100%,4rem);
+    .item{
+      @include wh(100%,2rem);
+      border-bottom: 1px solid #e6e6e6;
+    }
+  }
+</style>
